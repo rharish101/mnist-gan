59d5fd3201fed54fd7a05ed1db6d616ebe2ce46c
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-04-22 10:41:45 +0200

    Slight refactor of download script for readability

--- download_mnist.sh
+++ download_mnist.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 location="${1:-"./datasets/MNIST/"}"  # the first argument is where the dataset will be downloaded
-if [ ! -d "$location" ]; then
-    mkdir -p "$location"
-    pushd "$location" > /dev/null
-    curl --remote-name-all http://yann.lecun.com/exdb/mnist/{train-images-idx3,train-labels-idx1,t10k-images-idx3,t10k-labels-idx1}-ubyte.gz
-    gzip -d *.gz
-    echo "MNIST dataset saved in \"$(pwd)\""
-    popd > /dev/null
-else
+if [ -d "$location" ]; then
     echo "Path \"$location\" already exists"
     exit 1
 fi
+
+mkdir -p "$location"
+pushd "$location" > /dev/null
+curl --remote-name-all http://yann.lecun.com/exdb/mnist/{train-images-idx3,train-labels-idx1,t10k-images-idx3,t10k-labels-idx1}-ubyte.gz
+gzip -d *.gz
+echo "MNIST dataset saved in \"$(pwd)\""
+popd > /dev/null
